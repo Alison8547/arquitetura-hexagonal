9a76ef16d0599dd59953f92c7775c7df1db7828e
Alison8547 <alisonailson100@hotmail.com>
2023-10-13 19:09:49 -0300

    feat: Model Developer

---
+++ src/main/java/com/br/hexagonal/domain/model/Developer.java
@@ -0,0 +1,18 @@
+package com.br.hexagonal.domain.model;
+
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Developer {
+
+    private UUID id;
+    private String nameDeveloper;
+    private String email;
+    private String positionLevel;
+}

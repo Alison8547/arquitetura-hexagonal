a469bfdeea2ff55bd11a98fd90f67d75ae55a9c4
Alison8547 <alisonailson100@hotmail.com>
2023-10-14 00:55:58 -0300

    feat: add developer request

---
+++ src/main/java/com/br/hexagonal/entrypoint/rest/dto/request/DeveloperRequest.java
@@ -0,0 +1,26 @@
+package com.br.hexagonal.entrypoint.rest.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeveloperRequest {
+
+    @NotBlank
+    private String nameDeveloper;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String positionLevel;
+}

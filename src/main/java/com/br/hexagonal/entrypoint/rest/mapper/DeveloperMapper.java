ba720404fa2f6412c2b2e4683a958febdd5ebc04
Alison8547 <alisonailson100@hotmail.com>
2023-10-14 01:03:09 -0300

    feat: DeveloperMapper with modelMapper

--- src/main/java/com/br/hexagonal/entrypoint/rest/mapper/DeveloperMapper.java
+++ src/main/java/com/br/hexagonal/entrypoint/rest/mapper/DeveloperMapper.java
@@ -1,5 +1,8 @@
 package com.br.hexagonal.entrypoint.rest.mapper;
 
+import com.br.hexagonal.application.domain.Developer;
+import com.br.hexagonal.entrypoint.rest.dto.request.DeveloperRequest;
+import com.br.hexagonal.entrypoint.rest.dto.response.DeveloperResponse;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
@@ -10,6 +13,12 @@ public class DeveloperMapper {
 
     private final ModelMapper mapper;
 
+    public Developer toDeveloperModel(DeveloperRequest developerRequest) {
+        return mapper.map(developerRequest, Developer.class);
+    }
 
+    public DeveloperResponse toDeveloperResponse(Developer developer) {
+        return mapper.map(developer, DeveloperResponse.class);
+    }
 
 }

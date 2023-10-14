b0cba4066beebb16f25837df9dc5593950247986
Alison8547 <alisonailson100@hotmail.com>
2023-10-14 00:54:04 -0300

    feat: add task response

---
+++ src/main/java/com/br/hexagonal/entrypoint/rest/dto/response/TaskResponse.java
@@ -0,0 +1,23 @@
+package com.br.hexagonal.entrypoint.rest.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskResponse {
+
+    private UUID id;
+    private String title;
+    private String description;
+    private LocalDateTime timeOpen;
+    private LocalDateTime deadline;
+    private Boolean status;
+}

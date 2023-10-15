46775a77fa718e46be7ad6ebc240bb2584f498a6
Alison8547 <alisonailson100@hotmail.com>
2023-10-14 23:29:36 -0300

    feat: Add mappers core for entity

--- src/main/java/com/br/hexagonal/adapters/inbound/mapper/DeveloperMapper.java
+++ src/main/java/com/br/hexagonal/adapters/inbound/mapper/DeveloperMapper.java
@@ -3,6 +3,7 @@ package com.br.hexagonal.adapters.inbound.mapper;
 import com.br.hexagonal.adapters.inbound.dto.request.DeveloperRequest;
 import com.br.hexagonal.adapters.inbound.dto.response.DeveloperResponse;
 import com.br.hexagonal.adapters.outbound.entity.DeveloperEntity;
+import com.br.hexagonal.application.domain.Developer;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
@@ -13,12 +14,20 @@ public class DeveloperMapper {
 
     private final ModelMapper mapper;
 
-    public DeveloperEntity toDeveloperModel(DeveloperRequest developerRequest) {
+    public DeveloperEntity toDeveloperEntity(DeveloperRequest developerRequest) {
         return mapper.map(developerRequest, DeveloperEntity.class);
     }
 
-    public DeveloperResponse toDeveloperResponse(DeveloperEntity developer) {
-        return mapper.map(developer, DeveloperResponse.class);
+    public DeveloperResponse toDeveloperResponse(DeveloperEntity developerEntity) {
+        return mapper.map(developerEntity, DeveloperResponse.class);
+    }
+
+    public DeveloperEntity developerToDeveloperEntity(Developer developer) {
+        return mapper.map(developer, DeveloperEntity.class);
+    }
+
+    public Developer developerEntityToDeveloper(DeveloperEntity developerEntity) {
+        return mapper.map(developerEntity, Developer.class);
     }
 
 }
--- src/main/java/com/br/hexagonal/adapters/inbound/mapper/TaskMapper.java
+++ src/main/java/com/br/hexagonal/adapters/inbound/mapper/TaskMapper.java
@@ -3,6 +3,7 @@ package com.br.hexagonal.adapters.inbound.mapper;
 import com.br.hexagonal.adapters.inbound.dto.request.TaskRequest;
 import com.br.hexagonal.adapters.inbound.dto.response.TaskResponse;
 import com.br.hexagonal.adapters.outbound.entity.TaskEntity;
+import com.br.hexagonal.application.domain.Task;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
@@ -17,7 +18,15 @@ public class TaskMapper {
         return mapper.map(taskRequest, TaskEntity.class);
     }
 
-    public TaskResponse toTaskResponse(TaskEntity task) {
-        return mapper.map(task, TaskResponse.class);
+    public TaskResponse toTaskResponse(TaskEntity taskEntity) {
+        return mapper.map(taskEntity, TaskResponse.class);
+    }
+
+    public TaskEntity taskToTaskEntity(Task task) {
+        return mapper.map(task, TaskEntity.class);
+    }
+
+    public Task taskEntityToTask(TaskEntity taskEntity) {
+        return mapper.map(taskEntity, Task.class);
     }
 }

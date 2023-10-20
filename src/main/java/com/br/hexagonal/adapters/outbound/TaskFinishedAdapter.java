cd7d655603098bbcfe2d86548d258cdff30505b1
Alison8547 <alisonailson100@hotmail.com>
2023-10-19 21:26:56 -0300

    feat: validation method finishedTask

--- src/main/java/com/br/hexagonal/adapters/outbound/TaskFinishedAdapter.java
+++ src/main/java/com/br/hexagonal/adapters/outbound/TaskFinishedAdapter.java
@@ -5,6 +5,7 @@ import com.br.hexagonal.adapters.inbound.mapper.TaskMapper;
 import com.br.hexagonal.adapters.outbound.repository.TaskRepository;
 import com.br.hexagonal.application.domain.Task;
 import com.br.hexagonal.application.ports.out.TaskFinishedPort;
+import com.br.hexagonal.config.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
@@ -24,6 +25,10 @@ public class TaskFinishedAdapter implements TaskFinishedPort {
     @Override
     public Task finishedTask(UUID idTask) {
         TaskEntity taskEntity = getTaskForDeveloperAdapter.findTask(idTask);
+
+        if (taskEntity.getDeveloper() == null)
+            throw new BusinessException("Task cannot be completed because there is no one linked to it!");
+
         taskEntity.setStatus(FINISHED);
         taskRepository.save(taskEntity);
         log.info("Task finished Title: {}", taskEntity.getTitle());

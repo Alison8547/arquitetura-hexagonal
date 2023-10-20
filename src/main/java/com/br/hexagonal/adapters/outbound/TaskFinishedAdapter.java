9d90551ce223d0153d0a6f93e553391c8f8fae8b
Alison8547 <alisonailson100@hotmail.com>
2023-10-19 21:13:02 -0300

    feat: Task finished adapter

---
+++ src/main/java/com/br/hexagonal/adapters/outbound/TaskFinishedAdapter.java
@@ -0,0 +1,32 @@
+package com.br.hexagonal.adapters.outbound;
+
+import com.br.hexagonal.adapters.inbound.entity.TaskEntity;
+import com.br.hexagonal.adapters.inbound.mapper.TaskMapper;
+import com.br.hexagonal.adapters.outbound.repository.TaskRepository;
+import com.br.hexagonal.application.domain.Task;
+import com.br.hexagonal.application.ports.out.TaskFinishedPort;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Log4j2
+@Component
+@RequiredArgsConstructor
+public class TaskFinishedAdapter implements TaskFinishedPort {
+
+    private final TaskRepository taskRepository;
+    private final TaskMapper mapper;
+    private final GetTaskForDeveloperAdapter getTaskForDeveloperAdapter;
+    private static final boolean FINISHED = false;
+
+    @Override
+    public Task finishedTask(UUID idTask) {
+        TaskEntity taskEntity = getTaskForDeveloperAdapter.findTask(idTask);
+        taskEntity.setStatus(FINISHED);
+        taskRepository.save(taskEntity);
+        log.info("Task finished Title: {}", taskEntity.getTitle());
+        return mapper.taskEntityToTask(taskEntity);
+    }
+}

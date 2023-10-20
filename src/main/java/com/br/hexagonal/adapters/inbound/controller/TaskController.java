982d22f107bcf68cc30e355623c08d45c786a412
Alison8547 <alisonailson100@hotmail.com>
2023-10-19 21:25:01 -0300

    feat: TaskFinished endpoint controller

--- src/main/java/com/br/hexagonal/adapters/inbound/controller/TaskController.java
+++ src/main/java/com/br/hexagonal/adapters/inbound/controller/TaskController.java
@@ -5,16 +5,15 @@ import com.br.hexagonal.adapters.inbound.dto.response.TaskResponse;
 import com.br.hexagonal.adapters.inbound.mapper.TaskMapper;
 import com.br.hexagonal.application.domain.Task;
 import com.br.hexagonal.application.ports.in.CreateTaskUserCasePort;
+import com.br.hexagonal.application.ports.in.TaskFinishedUserCasePort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.UUID;
 
 @Validated
 @RestController
@@ -23,11 +22,17 @@ import javax.validation.Valid;
 public class TaskController {
 
     private final CreateTaskUserCasePort createTaskUserCasePort;
+    private final TaskFinishedUserCasePort taskFinishedUserCasePort;
     private final TaskMapper mapper;
 
     @PostMapping("/create-task")
     public ResponseEntity<TaskResponse> createTask(@Valid @RequestBody TaskRequest taskRequest) {
         Task task = mapper.toTask(taskRequest);
         return new ResponseEntity<>(mapper.toTaskResponse(createTaskUserCasePort.createTask(task)), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/finished-task/{id}")
+    public ResponseEntity<TaskResponse> finishedTask(@PathVariable(name = "id") UUID id) {
+        return new ResponseEntity<>(mapper.toTaskResponse(taskFinishedUserCasePort.finishedTask(id)), HttpStatus.OK);
     }
 }

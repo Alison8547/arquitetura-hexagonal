8eb123bcd1c0db4451c76fd95da454eb3d0678c6
Alison8547 <alisonailson100@hotmail.com>
2023-10-20 23:44:44 -0300

    feat: @Schema dtos

--- src/main/java/com/br/hexagonal/adapters/inbound/dto/request/DeveloperRequest.java
+++ src/main/java/com/br/hexagonal/adapters/inbound/dto/request/DeveloperRequest.java
@@ -1,5 +1,6 @@
 package com.br.hexagonal.adapters.inbound.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,12 +16,15 @@ import javax.validation.constraints.NotBlank;
 public class DeveloperRequest {
 
     @NotBlank
+    @Schema(description = "You name", example = "Alison")
     private String nameDeveloper;
 
     @NotBlank
     @Email
+    @Schema(description = "You email", example = "alison@hotmail.com")
     private String email;
 
     @NotBlank
+    @Schema(description = "You position level", example = "Junior")
     private String positionLevel;
 }
--- src/main/java/com/br/hexagonal/adapters/inbound/dto/request/TaskRequest.java
+++ src/main/java/com/br/hexagonal/adapters/inbound/dto/request/TaskRequest.java
@@ -1,5 +1,6 @@
 package com.br.hexagonal.adapters.inbound.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,11 +17,14 @@ import java.time.LocalDateTime;
 public class TaskRequest {
 
     @NotBlank
+    @Schema(description = "Title Task", example = "Kafka topic correction")
     private String title;
 
     @NotBlank
+    @Schema(description = "Description Task", example = "Change topic Kafka")
     private String description;
 
     @Future
+    @Schema(description = "Deadline Task", example = "2023-10-30T19:26:55")
     private LocalDateTime deadline;
 }

632d36d3614e2a908c3124be9292db6b131e99b7
Alison8547 <alisonailson100@hotmail.com>
2023-10-23 22:07:53 -0300

    feat:list finish tasks controller

--- src/main/java/com/br/hexagonal/adapters/inbound/controller/TaskController.java
+++ src/main/java/com/br/hexagonal/adapters/inbound/controller/TaskController.java
@@ -6,6 +6,7 @@ import com.br.hexagonal.adapters.inbound.mapper.TaskMapper;
 import com.br.hexagonal.application.domain.Task;
 import com.br.hexagonal.application.ports.in.CreateTaskUserCasePort;
 import com.br.hexagonal.application.ports.in.TaskFinishedUserCasePort;
+import com.br.hexagonal.application.ports.out.ListFinishTasksPort;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -16,6 +17,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.UUID;
 
 @Validated
@@ -26,6 +28,7 @@ public class TaskController {
 
     private final CreateTaskUserCasePort createTaskUserCasePort;
     private final TaskFinishedUserCasePort taskFinishedUserCasePort;
+    private final ListFinishTasksPort listFinishTasksPort;
     private final TaskMapper mapper;
 
 
@@ -56,5 +59,20 @@ public class TaskController {
     @PutMapping("/finished-task/{id}")
     public ResponseEntity<TaskResponse> finishedTask(@PathVariable(name = "id") UUID id) {
         return new ResponseEntity<>(mapper.toTaskResponse(taskFinishedUserCasePort.finishedTask(id)), HttpStatus.OK);
+    }
+
+    @Operation(summary = "List of finished tasks", description = "Return completed tasks from the database")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "List successfully"),
+                    @ApiResponse(responseCode = "400", description = "An exception was generated"),
+                    @ApiResponse(responseCode = "403", description = "You do not have permission to access this resource"),
+                    @ApiResponse(responseCode = "500", description = "An internal server exception was generated")
+            }
+    )
+    @GetMapping("/list-finished")
+    public ResponseEntity<List<TaskResponse>> listFinishTasks() {
+        return new ResponseEntity<>(listFinishTasksPort.listFinishTasks().stream()
+                .map(mapper::toTaskResponse).toList(), HttpStatus.OK);
     }
 }

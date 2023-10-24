6d94064b11e1cab0337cf5ba76f5749c8d959059
Alison8547 <alisonailson100@hotmail.com>
2023-10-23 22:38:07 -0300

    feat: new dto join dev

---
+++ src/main/java/com/br/hexagonal/adapters/inbound/dto/response/TaskDevResponse.java
@@ -0,0 +1,23 @@
+package com.br.hexagonal.adapters.inbound.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskDevResponse {
+    private UUID id;
+    private String title;
+    private String description;
+    private LocalDateTime timeOpen;
+    private LocalDateTime deadline;
+    private Boolean status;
+    private DeveloperResponse developer;
+}
